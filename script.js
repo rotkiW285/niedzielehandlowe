29d34db4d72de6ed2fb7de9d837510ef1a52df02
rotkiW285 <02.wybrzeza_kumple@icloud.com>
2023-11-30 18:33:15 +0100

    did stuff

--- script.js
+++ script.js
@@ -1,39 +1,67 @@
 var niedzieleHandlowe = [
-    "2023-06-25",
-    "2023-08-27",
-    "2023-12-17",
-    "2023-12-24"
-  ];
+  "2023-06-25",
+  "2023-08-27",
+  "2023-12-10",
+  "2023-12-17",
+  "2024-01-28",
+];
 
-  function czyNiedzielaHandlowa() {
-    var dzisiaj = new Date();
-    var rok = dzisiaj.getFullYear();
-    var miesiac = dzisiaj.getMonth() + 1; // Dodajemy 1, ponieważ miesiące są indeksowane od 0.
-    var dzien = dzisiaj.getDate();
+function czyNiedzielaHandlowa() {
+  var dzisiaj = new Date();
+  var rok = dzisiaj.getFullYear();
+  var miesiac = dzisiaj.getMonth() + 1; // Dodajemy 1, ponieważ miesiące są indeksowane od 0.
+  var dzien = dzisiaj.getDate();
 
-    var dzisiejszaData = rok + "-" + pad(miesiac) + "-" + pad(dzien); // Formatujemy datę jako "YYYY-MM-DD".
+  var dzisiejszaData = rok + "-" + pad(miesiac) + "-" + pad(dzien); // Formatujemy datę jako "YYYY-MM-DD".
 
-    if (niedzieleHandlowe.includes(dzisiejszaData)) {
-      return true;
-    } else {
-      return false;
+  if (niedzieleHandlowe.includes(dzisiejszaData)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Funkcja pomocnicza do dodawania zera przed liczbami jednocyfrowymi.
+function pad(number) {
+  return number < 10 ? "0" + number : number;
+}
+
+function znajdzNajblizszaNiedzieleHandlowa() {
+  var dzisiaj = new Date();
+  var dzisiajTimestamp = dzisiaj.getTime();
+  var najblizszaData = null;
+
+  niedzieleHandlowe.forEach(function (data) {
+    var dataTimestamp = new Date(data).getTime();
+    if (dataTimestamp > dzisiajTimestamp && (najblizszaData === null || dataTimestamp < new Date(najblizszaData).getTime())) {
+      najblizszaData = data;
     }
-  }
+  });
 
-  // Funkcja pomocnicza do dodawania zera przed liczbami jednocyfrowymi.
-  function pad(number) {
-    return number < 10 ? "0" + number : number;
-  }
+  if (najblizszaData) {
+    var najblizszaDateObj = new Date(najblizszaData);
+    var dzien = najblizszaDateObj.getDate();
+    var miesiac = najblizszaDateObj.getMonth() + 1;
+    var rok = najblizszaDateObj.getFullYear();
 
-  function sprawdzNiedzieleHandlowa() {
-    var wynik = "";
-    if (czyNiedzielaHandlowa()) {
-      document.querySelector("h1").classList.add("niedziela-handlowa");
-      wynik = "Dzisiaj jest niedziela handlowa!";
-    } else {
-      wynik = "Dzisiaj nie jest niedziela handlowa.";
-    }
-    document.write("<p>" + wynik + "</p>");
+    var formattedDate = pad(dzien) + "." + pad(miesiac) + "." + rok;
+    return formattedDate;
+  } else {
+    return null;
   }
+}
 
-  sprawdzNiedzieleHandlowa();
+function sprawdzNiedzieleHandlowa() {
+  var wynik = "";
+  var najblizszaData = znajdzNajblizszaNiedzieleHandlowa();
+
+  if (czyNiedzielaHandlowa()) {
+    document.querySelector("h1").classList.add("niedziela-handlowa");
+    wynik = "<p class='nh' style='color: lime;'>Dzisiaj jest niedziela handlowa! <br> Następna będzie " + najblizszaData + "</p>";
+  } else {
+    wynik = "<p class='nh' style='color: red;'>Nie, najbliższa niedziela handlowa jest " + najblizszaData + "</p>";
+  }
+  document.write(wynik);
+}
+
+sprawdzNiedzieleHandlowa();
